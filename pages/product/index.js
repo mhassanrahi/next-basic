a98f2905259e9a15a0351c0d0e6cd6e6b6f6dc97
Mehmood <m.hassan.rahi@gmail.com>
2021-08-31 23:19:20 +0500

    * Added product page

---
+++ pages/product/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Products = () => {
+    return (
+        <div>
+            <h3>Product 1</h3>
+            <h3>Product 2</h3>
+            <h3>Product 3</h3>
+        </div>
+    );
+};
+
+export default Products;
