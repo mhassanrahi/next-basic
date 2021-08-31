6551b0aa686fcb079328f4412a4f13e14d01bb9a
Mehmood <m.hassan.rahi@gmail.com>
2021-08-31 23:09:05 +0500

    * Added blog pages

---
+++ pages/blog/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Blog = () => {
+    return <div>All the blogs are to be displayed here.</div>;
+};
+
+export default Blog;
---
+++ pages/blog/second.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const First = () => {
+    return <div>Second Blog.</div>;
+};
+
+export default First;
