220f7c4898390b52de0d7754c0ddab3492f7f934
Mehmood <m.hassan.rahi@gmail.com>
2021-09-07 22:11:12 +0500

    * Added catch all routes feature

---
+++ pages/docs/[...params].js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const Docs = () => {
+    const router = useRouter();
+    const { params = [] } = router.query;
+    console.log(params);
+    if (params.length === 1) {
+        return <h3>Viewing docs for feature {params[0]}.</h3>;
+    } else if (params.length === 2) {
+        return (
+            <h3>
+                Viewing docs for feature {params[0]} and concept {params[1]}.
+            </h3>
+        );
+    }
+    return <h3>Docs Home</h3>;
+};
+
+export default Docs;
