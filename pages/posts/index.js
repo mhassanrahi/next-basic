ab9ea5c4af92ac0244a3a1c801a58494bbec6952
Mehmood <m.hassan.rahi@gmail.com>
2021-09-18 13:40:13 +0500

    * Added post page

---
+++ pages/posts/index.js
@@ -0,0 +1,23 @@
+import { Post } from "../../components/Post";
+
+function Posts({ posts }) {
+    return (
+        <>
+            <h1>List of Posts</h1>
+            <ul>
+                {posts.map((post) => (
+                    <Post post={post} key={post.id.toString()} />
+                ))}
+            </ul>
+        </>
+    );
+}
+export async function getStaticProps() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await res.json();
+
+    return {
+        props: { posts: posts.slice(0, 11) },
+    };
+}
+export default Posts;
