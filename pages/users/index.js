367849761941fb49fc8306f9ad457453d47dca82
Mehmood <m.hassan.rahi@gmail.com>
2021-09-18 11:21:59 +0500

    * Used getStaticProps method to pre-render with data

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-function Users() {
+function Users({ users }) {
     return (
         <>
             <h5>
@@ -10,8 +10,26 @@ function Users() {
                 </Link>
             </h5>
             <h1>List of users</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id.toString()}>{user.name}</li>
+                ))}
+            </ul>
         </>
     );
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+    // Call an external API endpoint to get users
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+
+    return {
+        props: {
+            users,
+        },
+    };
 }
 
 export default Users;
