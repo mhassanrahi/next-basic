ab88e30da96de7e901b27f4d08b3b1886c6b3283
Mehmood <m.hassan.rahi@gmail.com>
2021-09-18 13:40:49 +0500

    * Used getStaticPaths for dynamic ssg

---
+++ pages/posts/[postId].js
@@ -0,0 +1,40 @@
+function PostDetails({ post }) {
+    return (
+        <>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+        </>
+    );
+}
+
+export default PostDetails;
+
+export async function getStaticProps(context) {
+    const { params } = context;
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+    );
+    const post = await res.json();
+
+    return {
+        props: { post },
+    };
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { postId: "1" } },
+            { params: { postId: "2" } },
+            { params: { postId: "3" } },
+            { params: { postId: "4" } },
+            { params: { postId: "5" } },
+            { params: { postId: "6" } },
+            { params: { postId: "7" } },
+            { params: { postId: "8" } },
+            { params: { postId: "9" } },
+            { params: { postId: "10" } },
+        ],
+        fallback: false,
+    };
+}
